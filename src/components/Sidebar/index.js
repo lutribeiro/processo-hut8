b14bc0e5524173b0bbfa20cd625963a26fb0e59d
lutribeiro <lutribeiro13@gmail.com>
2020-11-16 15:24:53 -0300

    alterações

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css'
+
+function Sidebar (props){
+    return (
+        <div className="Sidebar">
+            <section class="grid center"> 
+            <img class = "avatar-fausto" src ="https://i.imgur.com/aU48TWI.jpg" alt = "Avatar Fausto Silva"></img>    
+            <h1>{props.name}</h1>       
+            </section>
+        </div>
+    )
+}
+export default Sidebar
