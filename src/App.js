8f551bb02c972cb75448098f3842a774530ed426
lutribeiro <lutribeiro13@gmail.com>
2020-11-19 17:42:54 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Sidebar from './components/Sidebar'
 import Resume from './components/Resume'
+import './App.css'
 
 function App() {
   return (
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -5,11 +5,13 @@ function Sidebar (props){
     return (
         <div className="Sidebar">
             <section class="grid center"> 
+            <div>
             <img class = "avatar-fausto" src ="https://i.imgur.com/aU48TWI.jpg" alt = "Avatar Fausto Silva"></img>    
             <h1>{props.name}</h1> 
             <h2>{props.course} </h2>
             <ul class= "button-dashboard"> <a href= "#">Dashboard</a></ul>
             <ul class= "button-courses"> <a href= "#">Meus cursos</a></ul>
+            </div>
             </section>
         </div>
     )
