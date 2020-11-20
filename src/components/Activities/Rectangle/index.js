35892ecd9badd5324260cb1c71b61028faa85040
lutribeiro <lutribeiro13@gmail.com>
2020-11-20 15:00:21 -0300

    activities changes

--- src/components/Activities/Rectangle/index.js
+++ src/components/Activities/Rectangle/index.js
@@ -5,13 +5,19 @@ function index (props){
     return (
       
         <div className = "rectangle">
-            <div className = "image">
-                <img>{props.image}</img>
-            </div>
+            {/* <div className = "image"> */}
+                {/* <a> <img>{props.image}</img> <a/> */}
+            {/* </div> */}
             <div className = "info-rectangle">
-                <h1>{props.subject}</h1>
-                <h2>{props.activity}</h2>
-                <h3>{props.date}</h3>
+                <div className="subject">
+                    <h1>{props.subject}</h1>
+                </div>
+                <div className="activity">
+                    <h2>{props.activity}</h2>
+                </div>
+                <div className="date">
+                     <h3>{props.date}</h3>
+                </div>
             </div>
         </div>
     )
--- src/components/Activities/index.js
+++ src/components/Activities/index.js
@@ -8,11 +8,14 @@ function index (){
         <div>
             <h1 className = "title" >Próximas atividades</h1>
             <div className="rectangle-box">
-                <Rectangle image = "https://i.imgur.com/aadwyfC.jpg" subject = "Algoritmos e Programção" activity ="Enviar arquivo Peter Smoke" date = "20/11/2020"> </Rectangle>
-                <Rectangle image = "https://i.imgur.com/RgQrlAS.jpg" subject = "Projeto de Banco de Dados" activity ="Enviar o esquema MySQL" date = "20/11/2020"> </Rectangle>
+                <Rectangle subject = "Algoritmos e Programção" activity ="Enviar arquivo Peter Smoke" date = "20/11/2020"> </Rectangle>
+                <Rectangle subject = "Projeto de Banco de Dados" activity ="Enviar o esquema MySQL" date = "20/11/2020"> </Rectangle>
             </div>
 
         </div>
     )
 }
 export default index
+
+// image = "https://i.imgur.com/aadwyfC.jpg"
+// image = "https://i.imgur.com/RgQrlAS.jpg" 
